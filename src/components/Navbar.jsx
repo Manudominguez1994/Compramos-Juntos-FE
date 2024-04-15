ad93657ca8eee8e81785b7b7bc1f81cdc54cdf06
Manudominguez1994 <manudomnav1994@gmail.com>
2024-04-15 19:12:41 +0200

    a

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -55,7 +55,7 @@ function MyNavbar({
           <div>
           <Navbar.Brand href="#home">
             <img
-              src="front-end/public/logo_compramos_juntos.png"
+              src="public/logo_compramos_juntos.png"
               width="30"
               height="30"
               className="d-inline-block align-top"
